f17309549b482ebd351ce010d45e705e604377a9
Chandril023 <work.Chandril@gmail.com>
2024-06-20 14:43:54 +0530

    version 0 some changes 1

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,6 @@ import withPlaiceholder from "@plaiceholder/next";
 const nextConfig = {
   output: 'standalone',
 
-  images: {
-    domains: ['images.ctfassets.net'],
-    loader: 'custom',
-    loaderFile: './lib/contentful-loader.tsx',
-    path: 'https://',
-  },
 }
 
 export default withPlaiceholder(nextConfig);
