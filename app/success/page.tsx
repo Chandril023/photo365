595ca7a557e3df190d11ef6c754bfffb2960e17a
Chandril023 <work.Chandril@gmail.com>
2024-06-21 23:00:15 +0530

    contact fully functional

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import Link from "next/link";
-
-interface SuccessPageProps {
-  message?: string; // Define props with optional 'message' of type string
-}
-
-const SuccessPage: React.FC<SuccessPageProps> = ({ message = "Success!" }) => {
+function SuccessPage(){
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-green-600 mb-8">{message}</h1>
+      <h1 className="text-4xl font-bold text-green-600 mb-8">Thank You ! We have received your message</h1>
       <Link href="/">
         <a className="text-blue-500 hover:underline">Go Back Home</a>
       </Link>
